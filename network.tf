114b279f4a0120d18014669108b0a085f2834889
Mitch <mitch.chan@westpac.com.au>
2022-03-16 03:39:10 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/mitchchan-training-2/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  name     = "${var.prefix}-workshop"
+  resource_group_name = "${var.prefix}-workshop"
 }
