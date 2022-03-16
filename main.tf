0be04f7f02215c9ff7215e187bc66b298d4a77cb
Mitch <mitch.chan@westpac.com.au>
2022-03-16 03:10:11 +0000

    readd both tags

--- main.tf
+++ main.tf
@@ -110,8 +110,6 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-  Department = "devops"
-  Billable = ""
 
   storage_image_reference {
     publisher = var.image_publisher
@@ -137,7 +135,10 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    Department = "devops"
+    Billable = ""
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
